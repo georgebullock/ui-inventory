7f6a49ab68b6c7d0dfb8dbd9c0f11111cc88bb8f
George Bullock <george.bullock@aroundhome.de>
2021-03-13 21:43:00 +0100

    Add types for styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {};
 
 function App() {
+	<ThemeProvider theme={theme}></ThemeProvider>;
 	return <div className="App">UI INVENTORY</div>;
 }
 
