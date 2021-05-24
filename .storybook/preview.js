396c306446fb1010ff8742fe6951093ec3cdd8fa
George Bullock <george.bullock@aroundhome.de>
2021-05-24 20:07:37 +0200

    Update validate script. Formatting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,17 @@
-import react from "react"
+import react from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './../src/theme/theme';
 import { GlobalStyles } from './../src/theme/globalStyles';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' }
+};
 
 export const decorators = [
-  (Story) => (
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Story />
-      </ThemeProvider>
-  ),
+	Story => (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<Story />
+		</ThemeProvider>
+	)
 ];
